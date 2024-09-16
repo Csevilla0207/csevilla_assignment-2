c1636b138acd82ad2c9192a3b13b7e05a6804951
Csevilla0207 <csevilla@rrc.ca>
2024-09-16 12:26:49 -0500

    Completed Lists, Tuples, Dictionaries, and Sets

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -32,10 +32,61 @@ print(f"Pre tax value : {Vehicle_Price} Provincial tax: {Provincial_Tax} Federal
 
 
 #LISTS
+numbers = list(range(1, 11))
+print(f"Data type of 'numbers': {type(numbers)}")
+print("list of numbers:" , numbers)
+
+name = "kimi" 
+numbers.insert(5, name)
+print("list after adding first name:" , numbers)
+
+numbers.remove(9)
+print("list after removing 9: " , numbers)
+
+Letters = ['A', 'B', 'C']
+Combined_List = numbers + Letters
+print("Combined list:", Combined_List)
+
 
 #TUPLES
+Provinces = ("Manitoba", "Alberta", "British Columbia", "Ontario")
+print(f"Data type of provinces: {type(Provinces)}")
+print("Tuples of provinces:" , Provinces)
 
 #DICTIONARIES
+Currency = {
+    'nickel': 0.05,
+    'dime': 0.10,
+    'quarter': 0.25
+}
+print(f"Data type of Currency: {type(Currency)}")
+print("Original dictionary" , Currency)
+
+Currency['nickel'] = 5
+Currency['dime'] = 10  
+Currency['quarter'] = 25
+print("Moddified dictionary with whole numbers:" , Currency)
+
+Currency['loonie'] = 100
+Currency['toonie'] = 200
+print("Finaly dictionary with loonie and toonie's added: " , Currency)
 
 #SETS
+even_numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+print(f"Data type of 'even_numbers': {type(even_numbers)}")
+print("Set of even numbers:", even_numbers)
 
+multiples_of_5 = {5, 10, 15, 20}
+print("Set of multiples of 5:", multiples_of_5)
+
+unique_values = even_numbers.union(multiples_of_5)
+print("Set of unique values from both sets:", unique_values)
+
+common_values = even_numbers.intersection(multiples_of_5)
+print("Set of common values (multiples of 2 and 5):", common_values)
+
+even_not_in_five = even_numbers.difference(multiples_of_5)
+print("Set of even numbers not in multiples of 5:", even_not_in_five)
+
+five_not_in_even = multiples_of_5.difference(even_numbers)
+print("Set of multiples of 5 not in even numbers:", five_not_in_even)
