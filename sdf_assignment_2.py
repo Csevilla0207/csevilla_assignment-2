e5a1e751e659c4f5b86d78928cd55cdf673668c6
Csevilla0207 <csevilla@rrc.ca>
2024-09-16 11:37:00 -0500

    completed simple data types

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -6,7 +6,17 @@ Date: 9/16/2024
 """
 
 #SIMPLE DATA TYPES
+name = "Kimi"
+print(f"value: {name} type: {type(name)}")
 
+valid_license = True
+print(f"value: {valid_license} type: {type(valid_license)}")
+
+current_year = 2024
+print(f"this year: {current_year} type: {type(current_year)}")
+
+plus_one_year = current_year + 1
+print(f"one year: {plus_one_year} type: {type (plus_one_year)}")
 
 #CALCULATIONS
 
